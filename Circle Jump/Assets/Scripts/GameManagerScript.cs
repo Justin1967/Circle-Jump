d801399556ee6fbfc3b3d57dba7c478d52974c74
Justin1967 <44942455+Justin1967@users.noreply.github.com>
2022-11-17 18:52:30 +0100

    Update GameManagerScript.cs

--- Circle Jump/Assets/Scripts/GameManagerScript.cs
+++ Circle Jump/Assets/Scripts/GameManagerScript.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    private float circleMinimumX = -2.0f;
+    private float circleMaximumX = 2.0f;
+
+    [SerializeField]
+    private GameObject circlePrefab;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float circleXPosition = Random.Range(circleMaximumX, circleMinimumX);
+        Instantiate(circlePrefab, new Vector3(circleXPosition, transform.position.y, transform.position.z), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
