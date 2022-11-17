b1c44d7602fefefbabd22794b4182a3632a81a09
Justin1967 <44942455+Justin1967@users.noreply.github.com>
2022-11-17 18:03:07 +0100

    Circle Jump Project

--- Circle Jump/Assets/Scripts/CircleScript.cs
+++ Circle Jump/Assets/Scripts/CircleScript.cs
@@ -6,22 +6,11 @@ public class CircleScript : MonoBehaviour
     [SerializeField]
     private Transform orbit;
 
-    //Reference Orbit Positon
-    [SerializeField]
-    private Transform orbitPosition;
-
-    //Reference Jumper
-    private GameObject jumper;
-
     //Rotation speed Orbit
     private float rotationSpeed = 100.0f;
 
     //Rotation direction
     private float rotationDirection;
-
-    //Variable to check if Jumper has collided with a circle
-    public bool hasLanded = false;
-
 
     //The size of the circle is between 0.6 and 1.0
     private float circleSize;
@@ -36,19 +25,5 @@ public class CircleScript : MonoBehaviour
     void Update()
     {
         orbit.transform.Rotate(rotationDirection * rotationSpeed * Time.deltaTime * Vector3.forward);
-
-        if (hasLanded)
-        {
-            jumper.transform.SetPositionAndRotation(orbitPosition.position, orbitPosition.rotation);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Jumper"))
-        {
-            hasLanded = true;
-            jumper = GameObject.FindGameObjectWithTag("Jumper");
-        }
     }
 }
--- Circle Jump/Assets/Scripts/JumperScript.cs
+++ Circle Jump/Assets/Scripts/JumperScript.cs
@@ -1,18 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class JumperScript : MonoBehaviour
 {
+    //Reference CircleScript
+    private CircleScript circleScript;
+
+    //Reference Orbit Position
+    private Transform orbitPosition;
+
+    //Jumper speed
+    private float jumpSpeed = 20.0f;
+
+    //Variable to check if Jumper has jumped off the circle
+    private bool hasJumped = false;
+
+    //Variable to check if Jumper has landed on a circle
+    private bool hasLanded = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        circleScript = GameObject.FindGameObjectWithTag("Circle").GetComponent<CircleScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Jump when SPACE is pressed
+        if (Input.GetKeyDown(KeyCode.Space) && !hasJumped)
+        {
+            hasJumped = true;
+            hasLanded = false;
+        }
+
+        JumperMovement();
+        RotateAroundCircle();
+    }
+
+    private void JumperMovement()
+    {
+        if (hasJumped)
+        {
+            transform.Translate(jumpSpeed * Time.deltaTime * Vector2.up);
+        }
+    }
+
+    private void RotateAroundCircle()
+    {
+        if (hasLanded)
+        {
+            transform.SetPositionAndRotation(orbitPosition.position, orbitPosition.rotation);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Circle"))
+        {
+            //Get reference OrbitPosition of the Circle
+            orbitPosition = other.gameObject.transform.GetChild(0).transform.GetChild(0);
+
+            hasJumped = false;
+            hasLanded = true;
+
+            //Set the OrbitPosition to the point where the Jumper landed
+
+
+
+        }
     }
 }
