832d83bf59ffd30314c8575d06c212a7cc958937
Justin1967 <44942455+Justin1967@users.noreply.github.com>
2022-11-17 18:28:44 +0100

    Update CircleScript.cs

--- Circle Jump/Assets/Scripts/CircleScript.cs
+++ Circle Jump/Assets/Scripts/CircleScript.cs
@@ -18,6 +18,8 @@ public class CircleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        circleSize = Random.Range(0.7f, 1.0f);
+        transform.localScale = new Vector3(circleSize, circleSize, 1);
         rotationDirection = Mathf.Pow(-1, Random.Range(1, 3));
     }
 
