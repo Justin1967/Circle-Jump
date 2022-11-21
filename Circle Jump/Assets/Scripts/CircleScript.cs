845e74a729bab46ee201d4f958c6d1870b6d230c
Justin1967 <44942455+Justin1967@users.noreply.github.com>
2022-11-21 13:59:00 +0100

    Circle Jump Project

--- Circle Jump/Assets/Scripts/CircleScript.cs
+++ Circle Jump/Assets/Scripts/CircleScript.cs
@@ -12,23 +12,40 @@ public class CircleScript : MonoBehaviour
     [SerializeField]
     private TextMeshPro numberOfOrbitsText;
 
-    private int numberOfOrbits;
+    public enum MODES { STATIC = 0, LIMITED = 1 }
 
-    private float rotationSpeed = 200.0f;
+    public int myMode;
 
-    private float rotationDirection;
+    private int numberOfOrbits; //only for LIMITED circles
 
-    private float circleSize;
+    private float rotationSpeed = 200.0f; //orbit's rotation speed
+
+    private float rotationDirection; //rotation direction - either -1 or 1
+
+    private float circleSize; //circle size between 0.7 and 1
 
     void Start()
     {
-        numberOfOrbits = Random.Range(2, 4);
+        var chooseMode = Random.Range(0, 2);
+
+        if (chooseMode == 0)
+        {
+            myMode = (int)MODES.STATIC;
+            numberOfOrbitsText.text = " ";
+        }
+        else
+        {
+            myMode = (int)MODES.LIMITED;
+            numberOfOrbits = Random.Range(2, 4);
+            numberOfOrbitsText.text = numberOfOrbits.ToString();
+        }
+
+
         SpriteRenderer circleSpriteRenderer = GetComponent<SpriteRenderer>();
         circleSpriteRenderer.color = ColorManagerScript.instance.circleColor[GameManagerScript.instance.indexColor];
         circleSize = Random.Range(0.7f, 1.0f);
         transform.localScale = new Vector3(circleSize, circleSize, 1);
         rotationDirection = Mathf.Pow(-1, Random.Range(1, 3));
-        numberOfOrbitsText.text = numberOfOrbits.ToString();
     }
 
     void Update()
